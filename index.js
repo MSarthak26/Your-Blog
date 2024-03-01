39c1747eb6063784a77a713d43ed5e0d6aa05138
sarthak <mohanirajsarthak@gmail.com>
2024-03-02 00:52:13 +0530

    added blogs in a json file

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-import express from "express"
-import bodyParser from "body-parser"
-import fs from "fs"
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const session = require('express-session');
+
 
 const app=express();
 const port=3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
+
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true,
+    // Add more options as needed
+}));
 
 app.use(bodyParser.urlencoded({ extended: true }))
 var blog=[];
@@ -16,9 +25,10 @@ var blogobj={
 
 var index=-1;
 var result
-function CreateBlog(Title,Content){
+function CreateBlog(Title,Content,username){
     this.Title=Title;
     this.Content=Content;
+    this.username=username;
 }
 
 function CreateUser(username,password){
@@ -80,6 +90,7 @@ app.get("/register",(req,res)=>{
 app.post("/home",(req,res)=>{
     var username=req.body.username;
     var password=req.body.password;
+   
     let userdata=[]
     try {
         userdata = JSON.parse(fs.readFileSync('user.json', 'utf8'));
@@ -91,9 +102,11 @@ app.post("/home",(req,res)=>{
     for (let i = 0; i < userdata.length; i++) {
         // Check if the current user's username matches the search query
         if (userdata[i].username === username && userdata[i].password === password) {
-            // Return the user object if found
+            req.session.username=req.body["username"];
             find=true;
-            res.render("index.ejs",{yourblog:blog});
+            console.log(req.session.username);
+            res.render("index.ejs",{yourblog:blog,username:req.session.username});
+            return;
 
         }
     }
@@ -105,50 +118,126 @@ app.post("/home",(req,res)=>{
 
 )
 
-app.get("/home",(req,res)=>{
-    res.render("index.ejs",{yourblog:blog})
+app.post("/create",(req,res)=>{
+    const uname=req.session.username;
+    console.log(uname);
+    var entry=new CreateBlog(`${req.body["title"]}`,`${req.body["blogcontent"]}`,`${uname}`)
+    let bloglist=[];
+    try{
+        bloglist=JSON.parse(fs.readFileSync("blogs.json",'utf-8'));
+        bloglist.push(entry);
+    }
+    catch(error){
+        console.log("error in reading file");
+    }
+
+    try{
+        let string=JSON.stringify(bloglist);
+        fs.writeFileSync("blogs.json",string,'utf-8');
+        console.log("successfully written");
+    }
+    catch(error){
+        console.log("error");
+   }
+    
+
+    // blog.push(entry)
+    res.redirect("/homeafter")
 })
 
-app.post("/create",(req,res)=>{
-    var entry=new CreateBlog(`${req.body["title"]}`,`${req.body["blogcontent"]}`)
-    blog.push(entry)
-    res.redirect("/home")
+app.get("/homeafter",(req,res)=>{
+    let bloglist=[];
+    try{
+        bloglist=JSON.parse(fs.readFileSync("blogs.json",'utf-8'));
+        console.log("success")
+    }
+    catch(error){
+        console.log(error)
+    }
+
+    res.render("index.ejs",{yourblog:bloglist,username:req.session.username})
 })
 
 app.get("/view",(req,res)=>{
-    res.render("view.ejs",{yourblog:blog })
+    let bloglist=[];
+    try{
+        bloglist=JSON.parse(fs.readFileSync("blogs.json",'utf-8'));
+        console.log("success")
+    }
+    catch(error){
+        console.log(error)
+    }
+    res.render("view.ejs",{yourblog:bloglist })
 })
 
 app.post("/edit",(req,res)=>{
-    if(blog.length > 0){
+    let bloglist=[];
+    try{
+        bloglist=JSON.parse(fs.readFileSync("blogs.json",'utf-8'));
+        console.log("success")
+    }
+    catch(error){
+        console.log(error)
+    }
+    if(bloglist.length > 0){
         
-        result = blog.find(({ Title }) => Title === req.body["id"]);
-        index=blog.indexOf(result)
+        result = bloglist.find(({ Title }) => Title === req.body["id"]);
+        index=bloglist.indexOf(result)
         console.log(index)
         if (result) {
             // Pass the result and its index to the edit page
-            res.render("edit.ejs", { yourblog: blog, index:index});
+            res.render("edit.ejs", { yourblog: bloglist, index:index});
         } else {
-            res.render("edit.ejs", { yourblog: blog });
+            res.render("edit.ejs", { yourblog: bloglist });
         }
     }
     else {
-        res.render("edit.ejs", { yourblog: blog });
+        res.render("edit.ejs", { yourblog: bloglist });
     }
 });
 
 app.post("/verify",(req,res)=>{
-    blog[index].Title=req.body["id"]
-    blog[index].Content=req.body["editedBlog"]
-    res.redirect("/")
+    let bloglist=[];
+    try{
+        bloglist=JSON.parse(fs.readFileSync("blogs.json",'utf-8'));
+        console.log("success")
+    }
+    catch(error){
+        console.log(error)
+    }
+    bloglist[index].Title=req.body["id"]
+    bloglist[index].Content=req.body["editedBlog"]
+    let string=JSON.stringify(bloglist);
+    try{
+        fs.writeFileSync("blogs.json",string,'utf-8');
+    }
+    catch(error){
+        console.log(error)
+    }
+    res.redirect("/homeafter")
 })
 
 app.post("/delete",(req,res)=>{
-    const dresult = blog.find(({ Title }) => Title === req.body["id"]);
-    const dindex=blog.indexOf(dresult);
+    let bloglist=[];
+    try{
+        bloglist=JSON.parse(fs.readFileSync("blogs.json",'utf-8'));
+        console.log("success")
+    }
+    catch(error){
+        console.log(error)
+    }
+    const dresult = bloglist.find(({ Title }) => Title === req.body["id"]);
+    const dindex=bloglist.indexOf(dresult);
 
-    blog.splice(dindex,1);
-    res.redirect("/home")
+    bloglist.splice(dindex,1);
+    let string=JSON.stringify(bloglist);
+    try{
+        fs.writeFileSync("blogs.json",string,'utf-8');
+    }
+    catch(error){
+        console.log(error)
+    }
+    res.redirect("/homeafter")
 })
 
 app.listen(port,()=>{
